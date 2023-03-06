2c8bd709ee92f0210ab15467fb38e91044cd794f
joker <nelsonmishra007@gmail.com>
2023-03-06 20:37:19 +0530

    stop is added

--- DiselEng.java
+++ DiselEng.java
@@ -2,5 +2,8 @@ public class DiselEng {
     public void start(){
         System.out.println("Disel engien start"); 
     }
+    public void stop(){
+        System.out.println("Disel engien stop");
+    }
     
 }
