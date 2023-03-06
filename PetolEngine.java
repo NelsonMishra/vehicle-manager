f7529679b57282ac5dfe35c1a3692da8cb6ac70b
joker <nelsonmishra007@gmail.com>
2023-03-06 20:36:07 +0530

    stop method added

--- PetolEngine.java
+++ PetolEngine.java
@@ -2,4 +2,7 @@ public class PetolEngine{
     public void start(){
         System.out.println("pertol engien start"); 
     }
+    public void stop(){
+        System.out.println("perto; engien stop");
+    }
 }
