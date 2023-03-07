4535d73076ffe1a0ed21cb6c6cf7290ecfedb6fe
joker <nelsonmishra007@gmail.com>
2023-03-07 19:51:20 +0530

    added

--- vehicle.java
+++ vehicle.java
@@ -13,3 +13,16 @@ public class Vehicle{
 
  }
 }
+
+
+
+
+
+// public class electricEngine{
+//     public void start(){
+//         System.out.println("electric engine start");
+//     }
+//     public void stop(){
+//         System.out.println("engine stop");
+//     }
+// }
