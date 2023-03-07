c59b1597488c367290d591aaffd4c997c1fc5966
joker <nelsonmishra007@gmail.com>
2023-03-07 19:30:24 +0530

    create electric engine

---
+++ electric-engine.java
@@ -0,0 +1,5 @@
+public class electric-engine{
+    public void start({
+        System.out.println("electric engine start");
+    })
+}
--- vehicle.java
+++ vehicle.java
@@ -6,9 +6,11 @@ public class Vehicle{
     public static void main(String[] args) {
         PetolEngine pE = new PetolEngine();
         DiselEng dE = new DiselEng();
+        electric-engine eN = new electric-engine();
 
             pE.start();  
             dE.start();
+            eN.start();
             
 
  }
